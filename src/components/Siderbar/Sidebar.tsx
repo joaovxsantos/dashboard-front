ff14216379bc55e1dbae431a210e914fd1bf08b8
joao.xavier <joaovitor55670@gmail.com>
2025-08-28 12:04:33 -0300

    remove unused import

--- src/components/Siderbar/Sidebar.tsx
+++ src/components/Siderbar/Sidebar.tsx
@@ -1,16 +1,15 @@
 import useIsMobile from "../../hooks/isMobile";
 import SideBarMobile from "./mobile/SidebarMobile";
 import { SideBarDesk } from "./desktop/SidebarDesk";
-import styles from './Sidebar.module.scss'
 
 export default function SideBar() {
 
     const isMobile = useIsMobile();
 
-   
+
 
     return (
-        <div className={styles["side-bar-container"]}>
+        <div className="side-bar-container">
             {isMobile ? <SideBarMobile /> : <SideBarDesk />}
         </div>
     )
